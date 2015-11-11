4f49a28431196b4039f930d665bf175827000db1
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-11-10 20:11:03 -0400

    Pie

---
+++ pie.php
@@ -0,0 +1,42 @@
+
+<aside id="footer-widgets">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h3 class="footer-widget-title">Misión</h3>
+                <p>Lorem ipsum Amet fugiat elit nisi anim mollit minim labore ut esse Duis ullamco ad dolor veniam velit.</p>
+            </div>
+            <div class="col-md-6">
+                <h3 class="footer-widget-title">Visión</h3>
+                <p>Lorem ipsum Amet fugiat elit nisi anim mollit minim labore ut esse Duis ullamco ad dolor veniam velit.</p>
+            </div>
+        </div> <!-- row -->
+    </div> <!-- container -->
+</aside> <!-- footer-widgets -->
+<footer id="footer">
+    <p>&copy; <?php echo date("Y")?>, Todos los Derechos Reservados.</p>
+</footer>
+
+</div> <!-- boxed -->
+</div> <!-- sb-site -->
+
+
+<div id="back-top">
+    <a href="#header"><i class="fa fa-chevron-up"></i></a>
+</div>
+
+
+<script src="<?php echo $folder?>js/core/vendors.js"></script>
+
+<script src="<?php echo $folder?>js/core/styleswitcher.js"></script>
+
+<!-- Syntaxhighlighter -->
+<script src="<?php echo $folder?>js/core/syntaxhighlighter/shCore.js"></script>
+<script src="<?php echo $folder?>js/core/syntaxhighlighter/shBrushXml.js"></script>
+<script src="<?php echo $folder?>js/core/syntaxhighlighter/shBrushJScript.js"></script>
+
+<script src="<?php echo $folder?>js/core/app.js"></script>
+<script src="<?php echo $folder?>js/core/index.js"></script>
+
+</body>
+</html>
