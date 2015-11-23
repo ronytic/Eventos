d139424e3e160efbcf942d83fc48cc8f848f326b
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-11-22 23:21:21 -0400

    Login Check

--- login/check.php
+++ login/check.php
@@ -7,6 +7,6 @@ if(!(isset($_SESSION["LoginSistema"]) && $_SESSION['LoginSistema']==1)){
 	include_once(RAIZ."funciones/url.php");
 	header("Location:".url_base().$directory."login/?u=".$_SERVER['PHP_SELF']);
 }else{
-	include_once(RAIZ."funciones/funciones.php");	
+	//include_once(RAIZ."funciones/funciones.php");	
 }
 ?>
